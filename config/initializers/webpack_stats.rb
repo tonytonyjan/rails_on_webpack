b7f18d4084fd85052a833c3137d13edf4125fe0a
Jian Weihang <tonytonyjan@gmail.com>
2016-02-09 22:54:21 +0800

    add config file

---
+++ config/initializers/webpack_stats.rb
@@ -0,0 +1,7 @@
+WebpackStats.configure do |config|
+  # true by default, false in Rails production environment
+  # config.reload = false
+
+  # `Rails.root.join('stats.json')` by default
+  # config.stats_path = '/path/to/stats.json'
+end
