121d991d7c0c26b27ca66d5e54f576e315f33c43
Jian Weihang <tonytonyjan@gmail.com>
2016-02-15 00:21:26 +0800

    preset stage-2 contains transform-object-rest-spread

--- webpack.config.js
+++ webpack.config.js
@@ -46,8 +46,7 @@ module.exports = {
       exclude: /node_modules/,
       loader: 'babel',
       query: {
-        presets: ['react', 'es2015'],
-        plugins: ['transform-object-rest-spread']
+        presets: ['react', 'es2015', 'stage-2']
       }
     }]
   },
