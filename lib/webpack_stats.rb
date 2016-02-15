cee1f0dedd53a1a9142a378a0a935f9f86ec58e3
Jian Weihang <tonytonyjan@gmail.com>
2016-02-16 00:30:52 +0800

    detect stats file

--- lib/webpack_stats.rb
+++ lib/webpack_stats.rb
@@ -1,6 +1,7 @@
 module WebpackStats
   REGEXP = /(.+?)(?:-([0-9a-f]{20,}))?\.(\w+)/
   RELOAD = !Rails.env.production?
+  STATS_PATH = Rails.root.join('stats.json')
 
   class << self
     def assets
@@ -15,11 +16,16 @@ module WebpackStats
 
     # load webpack stats file, process and store it.
     def load!
-      @stats = JSON.parse File.read Rails.root.join('stats.json')
-      @assets = {}
-      @stats['assets'].each do |asset|
-        full_name, name, hash, ext = REGEXP.match(asset['name']).to_a
-        @assets["#{name}.#{ext}"] = File.join(@stats['publicPath'], full_name)
+      if File.exists?(STATS_PATH)
+        @stats = JSON.parse File.read STATS_PATH
+        @assets = {}
+        @stats['assets'].each do |asset|
+          full_name, name, hash, ext = REGEXP.match(asset['name']).to_a
+          @assets["#{name}.#{ext}"] = File.join(@stats['publicPath'], full_name)
+        end
+      else
+        @stats = {}
+        @assets = {}
       end
     end
   end
